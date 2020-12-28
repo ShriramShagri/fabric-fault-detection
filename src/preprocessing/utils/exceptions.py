e881b317131f89449f6065729a1ba95ae2e5defc
ShriramShagri <shagrisrm@gmail.com>
2020-12-28 12:38:49 +0530

    exceptions

--- src/preprocessing/utils/exceptions.py
+++ src/preprocessing/utils/exceptions.py
@@ -17,3 +17,19 @@ class ImageFileNotFoundError(Error):
 class ImageProcessingError(Error):
     def __init__(self, message = "Image Not found in the specified Dataset path"):
         super().__init__(message=message)
+
+class DatasetDirectoryNotFoundError(Error):
+    def __init__(self, message = "Invalid dataset path"):
+        super().__init__(message=message)
+
+class DataPreprocessingError(Error):
+    def __init__(self, message = "Data Preprocessing cannot be done. Code error"):
+        super().__init__(message=message)
+
+class InvalidDatasetError(Error):
+    def __init__(self, message = "Invalid dataset"):
+        super().__init__(message=message)
+
+class PredictionError(Error):
+    def __init__(self, message = "Invalid dataset"):
+        super().__init__(message=message)
